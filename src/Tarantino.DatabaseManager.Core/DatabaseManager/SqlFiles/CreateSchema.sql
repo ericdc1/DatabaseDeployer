73e6f11d6d5d505f28c29a869425d928b60fbda6
Gediminas Backevicius <gediminas.backevicius@gmail.com>
2012-07-17 11:16:45 +0300

    Primary key was modified. SQL Azure doesn't support uncluster primary key.

--- src/Tarantino.DatabaseManager.Core/DatabaseManager/SqlFiles/CreateSchema.sql
+++ src/Tarantino.DatabaseManager.Core/DatabaseManager/SqlFiles/CreateSchema.sql
@@ -4,15 +4,15 @@ if not exists (select 1 from dbo.sysobjects where name = 'usd_AppliedDatabaseScr
 		[ScriptFile] [nvarchar](255) NOT NULL,
 		[DateApplied] [datetime] NOT NULL,
 		[Version] [int] NULL,
-	constraint [PK_usd_AppliedDatabaseScript] primary key nonclustered 
+	constraint [PK_usd_AppliedDatabaseScript] primary key clustered 
 	(
 		[ScriptFile] asc
-	) on [PRIMARY]
-	) on [PRIMARY]
+	)
+	)
 
-	create clustered index [IX_usd_DateApplied] ON [dbo].[usd_AppliedDatabaseScript] 
+	create index [IX_usd_DateApplied] ON [dbo].[usd_AppliedDatabaseScript] 
 	(
 		[DateApplied] asc
-	) on [PRIMARY]
+	)
 
 end
